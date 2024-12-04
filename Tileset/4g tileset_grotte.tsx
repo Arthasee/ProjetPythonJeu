eb9fcec83650b78a3c6bd3235d4f6c2bc6682dfe
AHADDAD AMIR <ahaddadamir06@gmail.com>
2024-12-04 03:32:09 +0100

    modification des tileset

--- Tileset/4g tileset_grotte.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="4g tileset_grotte" tilewidth="16" tileheight="16" tilecount="640" columns="16">
- <image source="../../Projet_Pokemon-youtube/Projet_Pokemon-main/assets/map/tileset/4g tileset_grotte.png" width="256" height="640"/>
-</tileset>
