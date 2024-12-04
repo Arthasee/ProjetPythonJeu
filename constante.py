966ceeb743dd222a87a79a2510e2161521ae8bb9
AHADDAD AMIR <ahaddadamir06@gmail.com>
2024-12-04 03:34:50 +0100

    modification taille de la fenetre et de la map

--- constante.py
+++ constante.py
@@ -1,11 +1,11 @@
 # Constantes
-GRID_WIDTH = 40
-GRID_HEIGHT = 32
-CELL_SIZE = 16
+GRID_WIDTH = 32
+GRID_HEIGHT = 24
+CELL_SIZE = 32
 TOTAL_HEIGHT = GRID_HEIGHT * CELL_SIZE #Permet avoir hauteur
-TOTAL_WIDTH = int(TOTAL_HEIGHT * 16/9) # Permet avoir largeur proportionnel au 16/9 format d'écran standard (bien que la carte soit en 48*40, permet d'intégrer ATH)
+TOTAL_WIDTH = int(TOTAL_HEIGHT * 16/9) # Permet avoir largeur proportionnel au 16/9 format d'écran standard (bien que la carte soit en 32*24, permet d'intégrer ATH)
 MAP_WIDTH = GRID_WIDTH * CELL_SIZE
-ATH_WIDTH = TOTAL_WIDTH - MAP_WIDTH
+ATH_WIDTH = TOTAL_WIDTH - MAP_WIDTH + 60
 MAX_DISTANCE = 6 # limite de déplacement
 FPS = 30
 WHITE = (255, 255, 255)
